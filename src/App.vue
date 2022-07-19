48817db6015fb741b2098f054f900004e7be0028
hjlooft <hjlooft@gmail.com>
2022-07-19 12:45:51 +0900

    saveAppChanges

--- src/App.vue
+++ src/App.vue
@@ -21,41 +21,41 @@ parser.ontext = function (t) {
 };
 
 
-parser.onclosetag = function(tagName){
+parser.onclosetag = function (tagName) {
   //if before first tag
   oCurParentNode = oCurParentNode.parentNode;
 }
 
 parser.onopentag = function (node) {
   // opened a tag.  node has "name" and "attributes"
-  const newNode = {name: node.name, text: ""}
+  const newNode = { name: node.name, text: "" }
   node
   let attr = node.attributes; //? 
   newNode.attr = {};
   Object.entries(node.attributes).forEach(
-        k => newNode.attr[k[0]] = k[1]
-  );    
-  if(attr.length > 0){
+    k => newNode.attr[k[0]] = k[1]
+  );
+  if (attr.length > 0) {
   }
 
-  if(!oDom){
+  if (!oDom) {
     newNode.parentNode = null;
     oDom = newNode;
   } else {
-    if(typeof(oCurParentNode.children)==='undefined'){
+    if (typeof (oCurParentNode.children) === 'undefined') {
       oCurParentNode.children = [];
     }
     newNode.parentNode = oCurParentNode;
     oCurParentNode.children.push(newNode);
-    
+
   }
-  oCurParentNode = newNode;  
+  oCurParentNode = newNode;
 };
 parser.onattribute = function (attr) {
   // an attribute.  attr has "name" and "value"
 };
 parser.onend = function () {
-      // parser stream is done, and ready to have more stuff written to it.
+  // parser stream is done, and ready to have more stuff written to it.
 
 };
 //const testString = '<docEl id="1">Hello, <who>world</who>!</docEl> id="2" name="world"';
@@ -66,13 +66,12 @@ parser.write(testString).close();
 </script>
 
 <template>
-<VApp>
-  <VMain>
-    <VBtn>Click me,</VBtn>
-  </VMain>
-</VApp>
+  <VApp>
+    <VMain>
+      <VBtn>Click me,</VBtn>
+    </VMain>
+  </VApp>
   <div>just for display</div>
-  <HelloWorld></HelloWorld>
   <ul>
     <li v-for="child in oDom.children">
       {{ child.name }}
